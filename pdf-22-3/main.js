7a80183e3b8b622a91e5c4242e4d21b233dff25e
david crespo <davidcrespo@iMac-de-david.local>
2023-07-10 20:08:21 +0200

    Ejercicios DOM

--- pdf-22-3/main.js
+++ pdf-22-3/main.js
@@ -1,3 +1,17 @@
 /*Dado el siguiente html y javascript. Utiliza el array para crear dinamicamente
 una lista ul > li de elementos en el div de html con el atributo
 data-function="printHere".*/
+
+const places = ["Gondor", "Mordor", "Rivendel", "La Comarca", "Nümenor"];
+
+const myUl = document.createElement ("ul");
+const print = document.querySelector('[data-function="printHere"]');
+
+for (const place of places){
+    const myLi = document.createElement ("li");
+    myLi.innerHTML = place;
+    myUl.appendChild(myLi);
+   
+}
+print.appendChild(myUl);
+document.body.appendChild(print);
