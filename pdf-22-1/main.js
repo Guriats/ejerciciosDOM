731d7afaeb92343c10bb93373df0ff7766f1a587
david crespo <davidcrespo@iMac-de-david.local>
2023-07-10 19:28:21 +0200

    Ejercicios DOM

--- pdf-22-1/main.js
+++ pdf-22-1/main.js
@@ -2,9 +2,22 @@
 que imprima cada uno de los albums.*/
 
 const albums = [
-"De Mysteriis Dom Sathanas",
 "Reign of Blood",
 "Ride the Lightning",
 "Painkiller",
 "Iron Fist",
 ];
+
+const myUl = document.createElement("ul");
+
+for (const album of albums){
+    const myLi = document.createElement("li");
+    myLi.innerHTML = album;
+    myUl.appendChild(myLi);
+}
+document.body.appendChild(myUl);
+
+
+
+
+
